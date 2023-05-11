782885e9434df21e713fcfde6d040a504599eea9
Winterblade49 <Winterblade49@gmail.com>
2023-05-11 17:20:40 -0600

    push check

---
+++ Project_exspression_alert/Java Script/Dictinary.js
@@ -0,0 +1,10 @@
+function my_Dictinary(){
+    var Animal = {
+        species:"Dog",
+        Color:"Black",
+        Breed:"lab",
+        Age:4,
+        Sound:"Bark"
+    };
+    document.getElementById("Ani").innerHTML = Animal.Sound;
+}
