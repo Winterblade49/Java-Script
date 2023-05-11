1d6bf4cb708d971d6b8bec505c2c454550f8125f
Winterblade49 <Winterblade49@gmail.com>
2023-05-11 16:38:58 -0600

    AAAAAAAAA

--- Project_exspression_alert/Java Script/Math.js
+++ Project_exspression_alert/Java Script/Math.js
@@ -12,3 +12,45 @@ function subtract_Function() {
     var Sub = 5-2;
     document.getElementById("Math1").innerHTML="5-2= "+Sub;
 }
+
+function multi_Function() {
+    var Sub = 5*2;
+    document.getElementById("Math2").innerHTML="5*2= "+Sub;
+}
+
+function divide_Function() {
+    var Sub = 5/2;
+    document.getElementById("Math3").innerHTML="5/2= "+Sub;
+}
+
+function multipl_Function() {
+    var Sub = 5/2*10+12;
+    document.getElementById("Math4").innerHTML="5/2*10+12= "+Sub;
+}
+
+function MOD1_Function() {
+    var Sub = 25%6;
+    document.getElementById("MOD1").innerHTML="25%6= "+ -Sub;
+}
+
+function MOD_Function() {
+    var Sub = 25%6;
+    document.getElementById("MOD").innerHTML="25%6= "+Sub;
+}
+
+var I =5;
+I++
+document.write(I)
+var IO = 5;
+IO--
+document.write(i)
+
+window.alert(Math.random());
+window.alert(Math.random()*100)
+
+
+
+
+
+
+
