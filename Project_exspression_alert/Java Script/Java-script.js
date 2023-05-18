561998cbf205a3b095049d05ff84304a3697422a
Winterblade49 <Winterblade49@gmail.com>
2023-05-17 21:10:20 -0600

    Added Timer

--- Project_exspression_alert/Java Script/Java-script.js
+++ Project_exspression_alert/Java Script/Java-script.js
@@ -249,7 +249,22 @@ document.getElementById("Contcatenate").innerHTML = myString;
 }
 
 
+//Timer Function
+function countdown(){
+    var seconds = document.getElementById("seconds").value;
+    tick()
 
+    function tick(){
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        var time = setTimeout(tick, 1000);
+        if(seconds == -1) {
+            alert("Times up");
+            clearTimeout(time);
+            timer.innerHTML ="";
+        }
+    }
+}
 
 
 
